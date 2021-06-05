1a61977d5d1819fcfc2418b1484c94291c41e5f8
Jeppe Ram Pedersen <jeppe.ram.pedersen@gmail.com>
2021-06-05 11:29:37 +0200

    test

--- RADFS/Program.fs
+++ RADFS/Program.fs
@@ -1,9 +1,12 @@
 ﻿open System
 
+let a_ms_bin_string = "0001011100010100100111110100001110000100001001000010100111101101"
+let a_ms = 1663129274735143405UL
+
 // Opg.1a
-let multiply_shift (x:int64) (a:int64) (l:int) : int64 =
-    if ((a % 2L = 1L) || (l > 0) || (l < 64)) then
-        (a * x) >>> (64-l)
+let multiply_shift (x:uint64) (l:int): int64 =
+    if ((l > 0) || (l < 64)) then
+        (a_ms * x) >>> (64-l)
     else
         printfn "Invalid"
         0L
@@ -18,6 +21,20 @@ let pStart = bigint (2.0**qStart)
 let p = pStart - 1I
 let q = int qStart
 
+let multiply_mod_prime (x:int64) (a:bigint) (b:bigint) (l:int) : bigint =
+    if ((a < p) || (b < p) || (l > 0) || (l < 64)) then
+        let hx1 = (a * (bigint x) + b)
+        let hx2 = (hx1&&&p) + (hx1>>>q)
+        let hx3 =
+            if (hx2 >= p) then
+                hx2 - p
+            else
+                hx2
+        let hx4 = hx3 % (bigint (2.0**(float l) ))
+        hx4
+    else
+        0I
+
 let aBinString = "00010011101101110111001110000100110111010111001110000010110101000001001100100001000000011"
 let aDecString = "47671829177093929184936451"
 let aRan = System.Numerics.BigInteger.Parse(aDecString)
@@ -26,36 +43,50 @@ let bBinString = "10101110111000000011010010100001110010001101011110010001010001
 let bDecString = "422823747187209528456790390"
 let bRan = Numerics.BigInteger.Parse(bDecString)
 
-let multiply_mod_prime (a:bigint) (b:bigint) (x:bigint) (l:float) : bigint =
-    if (1=1) then
-        let hx1 = (a * x + b)
-        let hx2 = (hx1&&&p) + (hx1>>>q)
-        let hx3 =
-            if (hx2 >= p) then
-                hx2 - p
-            else
-                hx2
-        let TL = bigint (2.0**l)
-        let hx4 = hx3&&&(TL-1I)
-        hx4
-    else
-        0I
+//Opg1c
+let createStream (n : int) (l : int) : seq<uint64 * int> =
+    seq {
+        // We generate a random uint64 number.
+        let rnd = System.Random ()
+        let mutable a = 0UL
+        let b : byte [] = Array.zeroCreate 8
+        rnd.NextBytes (b)
+        let mutable x : uint64 = 0UL
+        for i = 0 to 7 do
+            a <- (a <<< 8) + uint64(b.[i])
 
-//for i = 12500 to 12700 do
-//    printfn ("%A") (multiply_mod_prime aRan bRan (bigint i) 11.0)
+        // We demand that our random number has 30 zeros on the least
+        // significant bits and then a one.
+        a <- (a ||| ((1UL<<<31) - 1UL)) ^^^ ((1UL<<<30) - 1UL)
 
-let xTest = 12247835I
-let lTest = 23.0
+        let mutable x = 0UL
+        for i = 1 to (n/3) do
+            x <- x + a
+            yield (x &&& (((1UL<<<l) - 1UL)<<<30) , 1)
+
+        for i = 1 to ((n + 1) /3) do
+            x <- x + a
+            yield (x &&& (((1UL<<<l) - 1UL)<<< 30) , -1)
+        
+        for i = 1 to (n + 2) /3 do
+            x <- x + a
+            yield (x &&& (((1UL<<<l) - 1UL)<<< 30) , 1)
+    }
 
 let timer = System.Diagnostics.Stopwatch.StartNew()
+
 timer.Start()
-multiply_shift (int64 xTest) (int64 3426234)  (int lTest)
+multiply_shift (int64 xTest) (int64 3426231)  (int lTest)
 timer.Stop()
 printfn "%A" (timer.Elapsed)
 
 timer.Reset()
 
 timer.Start()
-multiply_mod_prime aRan bRan xTest lTest
+multiply_mod_prime xTest aRan bRan lTest
 timer.Stop()
 printfn "%A" (timer.Elapsed)
+
+//Opg2a
+let make_hash_table (h : (int64 int) -> bigint) (l : int) : int =
+    1
